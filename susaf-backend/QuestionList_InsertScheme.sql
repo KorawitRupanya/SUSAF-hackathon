2f84fca7e646608a4fbbedec901ee4885a7dc6e1
cinapr <115988325+cinapr@users.noreply.github.com>
2023-04-24 17:57:06 +0300

    Add files via upload
    Question List from SusAF Framework

---
+++ susaf-backend/QuestionList_InsertScheme.sql
@@ -0,0 +1,111 @@
+INSERT INTO `questions`(`Questions_ID`, `Questions_Header`, `Prompt`, `Questions`) VALUES 
+('S1','SOCIAL','Sense of community means the feeling of belong to an organization, to an area or to a group of like-minded people.','How can the product or service affect a person sense of belonging to these groups?'),
+
+('S2','SOCIAL','Trust means having a firm belief in the reliability, truth, or ability of someone or something.','How can the product or service change the trust between the users and the business that owns the system?'),
+
+('S3','SOCIAL','Equity means the quality of being fair and impartial.','How can the system make people be treated differently from each other? (think data analytics or decision support)'),
+
+('S4','SOCIAL','Inclusiveness and diversity refers to the inclusion of people who might otherwise be excluded or marginalized.','How can the product or service impact on how people perceive others?'),
+
+('S5','SOCIAL','Inclusiveness and diversity refers to the inclusion of people who might otherwise be excluded or marginalized.','What effects can it have on users with different backgrounds, age groups,education levels, or other differences?'),
+
+('S6','SOCIAL','Participation and communication refers to imparting or interchanging thoughts, opinions or information by speech, writing, or signs.','How can the product or service change the way people: create networks; participate in group work; support, criticize or argue with others?');
+
+
+
+INSERT INTO `questions`(`Questions_ID`, `Questions_Header`, `Prompt`, `Questions`) VALUES ('I1','INDIVIDUAL','Health means the state of a person mental or physical condition.','How can the product or service improve or worsen a person physical, mental,and/or emotional health?(For example, can it make a person feel anything good or bad - e.g.(under)valued, (dis)respected, (in)dependent, or coerced?)'),
+
+('I2','INDIVIDUAL','Lifelong learning means the use of learning opportunities throughout people lives for continuous development.','How can the product or service affect people competencies?'),
+
+
+('I3','INDIVIDUAL','Privacy means being free from intrusion or disturbance in one private life.','How can the product or service expose (or help to hide) a person identity, whereabouts or relations?'),
+
+('I4','INDIVIDUAL','Safety means being protected from danger, risk, or injury.','How can the product or service expose (or protect) a person from physical harm?'),
+
+('I5','INDIVIDUAL','Safety means being protected from danger, risk, or injury.','How can it make a person feel more (or less) exposed to harm?'),
+
+('I6','INDIVIDUAL','Safety means being protected from danger, risk, or injury.','What if used in an unintended way?'),
+
+('I7','INDIVIDUAL','Agency means the capacity of an individual to act or make decisions of their free will.','How can the product or service empower (or prevent) a person from taking an action/ decision when necessary?'),
+
+('I8','INDIVIDUAL','Agency means the capacity of an individual to act or make decisions of their free will.','Can those affected by the product or service understand its implications, express concerns or be represented by someone?');
+
+
+
+
+
+
+
+
+
+
+INSERT INTO `questions`(`Questions_ID`, `Questions_Header`, `Prompt`, `Questions`) VALUES ('V1','ENVIRONMENTAL','Material and resources includes everything that is needed to produce, deploy, operate, and cease a product or service.','How are materials consumed to produce the product or service?'),
+
+('V2','ENVIRONMENTAL','Material and resources includes everything that is needed to produce, deploy, operate, and cease a product or service.','What about operating the product or service? E.g., requires hardware.'),
+
+('V3','ENVIRONMENTAL','Material and resources includes everything that is needed to produce, deploy, operate, and cease a product or service.','How can it change the way people consume material ? E.g., encourage to buy more?'),
+
+('V4','ENVIRONMENTAL','Waste & pollution means effects the product or service might have on soil, atmospheric, and water pollution.','How can producing parts or supplies generate waste or emissions ?'),
+
+('V5','ENVIRONMENTAL','Waste & pollution means effects the product or service might have on soil, atmospheric, and water pollution.','How can the use itself produce waste or emissions ?'),
+
+('V6','ENVIRONMENTAL','Waste & pollution means effects the product or service might have on soil, atmospheric, and water pollution.','How can it influence how much waste or emissions are generated?'),
+
+('V7','ENVIRONMENTAL','Waste & pollution means effects the product or service might have on soil, atmospheric, and water pollution.','How can it promote (or impair) recycling ?'),
+
+('V8','ENVIRONMENTAL','Biodiversity includes the effects of a product or service on biodiversity in its operational environment and other affected land.','How can it impact the plants or animals around it ? Or elsewhere ?'),
+
+('V9','ENVIRONMENTAL','Biodiversity includes the effects of a product or service on biodiversity in its operational environment and other affected land.','How can it change the composition of the soil around it? E.g., occupying / cropland ? What about elsewhere ?'),
+
+('V10','ENVIRONMENTAL','Biodiversity includes the effects of a product or service on biodiversity in its operational environment and other affected land.','How can it change the composition of the soil around it? E.g., occupying / cropland ? What about elsewhere ?'),
+
+('V11','ENVIRONMENTAL','Energy means all energy use that results from producing and using a product or service.','How can the product of service affect the need for production of energy ?'),
+
+('V12','ENVIRONMENTAL','Energy means all energy use that results from producing and using a product or service.','What about the use of energy ? E.g. encourages less energy.'),
+
+('V13','ENVIRONMENTAL','Energy means all energy use that results from producing and using a product or service.','Does the hardware run on renewable energy? Is there a way to incentivise that?'),
+
+('V14','ENVIRONMENTAL','Logistics means the effects of the product or service on moving people and/or goods.','How can it affect the need (and distance) for moving people or goods? How can it affect the means by which people or goods move?');
+
+
+INSERT INTO `questions`(`Questions_ID`, `Questions_Header`, `Prompt`, `Questions`) VALUES ('E1','ECONOMIC','Value means the worth, or usefulness of something, principles or standards; judgement of what is important in life.','How can the product or service create or destroy monetary value ? For whom ?'),
+
+('E2','ECONOMIC','Value means the worth, or usefulness of something, principles or standards; judgement of what is important in life.','Are there any other related types of business value? For whom?'),
+
+('E3','ECONOMIC','Customer Relationship Management steers a company interaction with current and potential customers to improve business relationships (e.g. retention, growth).','How can the product or service affect the relationship between the business and its customers?'),
+
+('E4','ECONOMIC','Customer Relationship Management steers a company interaction with current and potential customers to improve business relationships (e.g. retention, growth).','How can it enable co-creation or co-destruction of value ?'),
+
+('E5','ECONOMIC','Customer Relationship Management steers a company interaction with current and potential customers to improve business relationships (e.g. retention, growth).','How can it impact the financial situation of their customers & others?'),
+
+('E6','ECONOMIC','Supply Chain means a system of organisations, people, activities, information, and resources involved in moving a product or service from supplier to customer..','How can the product or service affect the supply chain of the business who owns it ?'),
+
+('E7','ECONOMIC','Supply Chain means a system of organisations, people, activities, information, and resources involved in moving a product or service from supplier to customer..','How can these changes in supply chain impact the financial situation?'),
+
+('E8','ECONOMIC','Supply Chain means a system of organisations, people, activities, information, and resources involved in moving a product or service from supplier to customer..','How can it impact the financial situation of their customers & others?'),
+
+('E9','ECONOMIC','Governance means the processes of interaction and decision-making among the actors involved in a system through the laws, norms, power or language of an organized society.','How can the product or service affect how and by whom such decisions are made and how does it affect the communication channels by which the relationships take place?'),
+('E10','ECONOMIC','Governance means the processes of interaction and decision-making among the actors involved in a system through the laws, norms, power or language of an organized society.','How can these changes impact the financial situation of the business and partners?'),
+
+('E11','ECONOMIC','Innovation refers to something new or to a change made to an existing product, idea, or field.','Do (parts of) the product or service affect the investment on research & development?'),
+
+('E12','ECONOMIC','Innovation refers to something new or to a change made to an existing product, idea, or field.','How can changes in innovation and R&D impact the financial situation?'),
+
+('E13','ECONOMIC','Innovation refers to something new or to a change made to an existing product, idea, or field.','Can it also impact the financial situation of their customers & others?');
+
+INSERT INTO `questions`(`Questions_ID`, `Questions_Header`, `Prompt`, `Questions`) VALUES ('T1','TECHNICAL','Maintainability means the degree to which an application is understood, repaired, or enhanced.','How are the operating system and runtime environment expected to change what does that required from maintainers of this system?'),
+
+('T2','TECHNICAL','Maintainability means the degree to which an application is understood, repaired, or enhanced.','How can the correctness of the system be affected by other systems or affect the correctness of others?'),
+
+('T3','TECHNICAL','Usability means the ability of users to productively use the system for the intended purpose.','What kind of knowledge or physical properties are required to use the system and how can this affect different types of users? For example, is good eyesight and small, sensitive hands required to operate a system on a small handheld device?'),
+
+('T4','TECHNICAL','Adaptability means the ability of a system to adapt itself to fit its behaviour according to changes in its environment or in parts of the system itself.','How could someone want to use the system in another context ? What can make that easier/more difficult?'),
+
+('T5','TECHNICAL','Adaptability means the ability of a system to adapt itself to fit its behaviour according to changes in its environment or in parts of the system itself.','What can make that easier/more difficult for the system to adapt itself to fit new usage scenarios?'),
+
+('T6','TECHNICAL','Security means freedom from, or resilience against, potential harm (or other unwanted coercive change) caused by external or internal attacks.','Which assets controlled by this system would be desirable to an attacker? E.g. financial information, people whereabouts or preferences, etc. What are the risks associated with these assets?'),
+
+('T7','TECHNICAL','Security means freedom from, or resilience against, potential harm (or other unwanted coercive change) caused by external or internal attacks.','What are other likely vulnerabilities of the system?'),
+('T8','TECHNICAL','Scalability means the system ability to handle growing amounts of work in a graceful manner or to be enlarged horizontally or vertically and will continue to function with comparable response times.','How can the system support changes in workload?'),
+
+('T9','TECHNICAL','Scalability means the system ability to handle growing amounts of work in a graceful manner or to be enlarged horizontally or vertically and will continue to function with comparable response times.','What can make that easier/more difficult?');
